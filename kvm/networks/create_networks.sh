96f29bfd26c29506285eff56ed2d69e076b5e0e8
Erlon R. Cruz <erlon@canonical.com>
2025-06-19 12:08:42 -0300

    New network configuration scripts
    Signed-off-by: Erlon R. Cruz <erlon@canonical.com>

--- kvm/networks/create_networks.sh
+++ kvm/networks/create_networks.sh
@@ -1,16 +1,85 @@
 #!/bin/bash
-# 10:oam, 20:admin, 30:public, 40:internal, 50:ext, 60:k8s
-HOSTNAME=$(hostname)
-if [ ${HOSTNAME} == "romano" ]; then
-    NETDEV=${NETDEV:-ens13f1np1}
-else
-    if [ ${HOSTNAME} == "x1" ]; then
-        NETDEV=${NETDEV:-enx000ec6c38af4}
-    else
-        echo "Can not run in host: ${HOSTNAME}"
-        exit 1
-    fi
+
+# Default configuration
+declare -A DEVICE_MAP  # Maps bridges to their physical devices
+HOST_IP_MAP=""
+BRIDGES=("oam:10" "admin:20" "public:30" "internal:40" "ext:50" "k8s:60")
+NETWORK_PREFIX="10.10"
+XML_TEMPLATE_FILE="$(dirname "$0")/network-template.xml"
+XML_OUTPUT_DIR="."
+
+# Help function
+show_help() {
+    echo "Usage: $0 [options]"
+    echo "Options:"
+    echo "  -d, --device MAP          Map physical device to bridge(s) (e.g., eth0:public,admin or eth1:oam)"
+    echo "  -h, --host-ip HOST:IP     Map hostname to IP (e.g., romano:1,x1:3,host2:5)"
+    echo "  -b, --bridge NAME:ID      Add bridge with VLAN ID (e.g., oam:10,admin:20)"
+    echo "  -p, --prefix PREFIX       Network prefix (default: 10.10)"
+    echo "  -o, --output-dir DIR      Directory to store generated XML files (default: current directory)"
+    echo "  --help                    Show this help message"
+    exit 0
+}
+
+# Parse command line arguments
+while [[ $# -gt 0 ]]; do
+    key="$1"
+    case $key in
+        -d|--device)
+            NETDEV="$2"
+            shift 2
+            ;;
+        -h|--host-ip)
+            HOST_IP_MAP="$2"
+            shift 2
+            ;;
+        -b|--bridge)
+            IFS=',' read -r -a BRIDGES <<< "$2"
+            shift 2
+            ;;
+        -p|--prefix)
+            NETWORK_PREFIX="$2"
+            shift 2
+            ;;
+        -o|--output-dir)
+            XML_OUTPUT_DIR="$2"
+            shift 2
+            ;;
+        --help)
+            show_help
+            ;;
+        *)
+            echo "Unknown option: $1"
+            show_help
+            exit 1
+            ;;
+    esac
+done
+
+# Validate required parameters
+if [ -z "$HOST_IP_MAP" ]; then
+    echo "Error: Host to IP mapping not specified"
+    echo "Use -h or --host-ip to map hostnames to IP addresses"
+    exit 1
 fi
+
+# Parse host IP mapping
+declare -A HOST_IPS
+IFS=',' read -ra MAPPINGS <<< "$HOST_IP_MAP"
+for map in "${MAPPINGS[@]}"; do
+    IFS=':' read -r host ip <<< "$map"
+    HOST_IPS["$host"]="$ip"
+done
+
+# Get current hostname
+HOSTNAME=$(hostname)
+if [ -z "${HOST_IPS[$HOSTNAME]}" ]; then
+    echo "Error: No IP mapping found for host: $HOSTNAME"
+    echo "Available mappings: ${!HOST_IPS[*]}"
+    exit 1
+fi
+
+HOST_IP=${HOST_IPS[$HOSTNAME]}
 
 # Function to check if VLAN exists
 vlan_exists() {
@@ -38,10 +107,37 @@ virsh_net_exists() {
     return $?
 }
 
+# Function to create XML network configuration file
+create_network_xml() {
+    local bridge_name="$1"
+    local output_file="${XML_OUTPUT_DIR}/net-${bridge_name}.xml"
+    local network_name="${bridge_name}"
+    
+    # Check if template file exists
+    if [ -f "$XML_TEMPLATE_FILE" ]; then
+        # Use template and replace placeholders
+        sed "s/__NETWORK_NAME__/${network_name}/g; s/__BRIDGE_NAME__/${network_name}/g" "$XML_TEMPLATE_FILE" > "$output_file"
+        echo "Generated XML file from template: $output_file"
+        return 0
+    else
+        echo "Error: XML template file not found at $XML_TEMPLATE_FILE"
+        exit 1
+    fi
+}
+
 # Create network vlans
-for id in $(seq 10 10 60); do
+for bridge in "${BRIDGES[@]}"; do
+    IFS=':' read -r br id <<< "$bridge"
+    
+    # Skip VLAN creation if no device is mapped to this bridge
+    if [ -z "${DEVICE_MAP[$br]}" ]; then
+        echo "No physical device mapped to bridge $br, skipping VLAN creation"
+        continue
+    fi
+    
+    NETDEV=${DEVICE_MAP[$br]}
     if ! vlan_exists $id; then
-        echo "Creating VLAN $id"
+        echo "Creating VLAN $id for bridge $br on device $NETDEV"
         sudo ip link add link ${NETDEV} name vlan${id} type vlan id ${id}
         sudo ip link set dev vlan${id} up
     else
@@ -53,34 +149,29 @@ for id in $(seq 10 10 60); do
     fi
 done
 
-id=10
-for br in oam admin public internal ext k8s; do
-    NET_NAME="maas-${br}"
+# Create bridges and configure networks
+for bridge in "${BRIDGES[@]}"; do
+    IFS=':' read -r br id <<< "$bridge"
+    NET_NAME="${br}"
 
-    # Determine the correct IP address based on hostname
-    # Modified to use romano as the primary hostname with IP .1
-    if [ ${HOSTNAME} == "romano" ]; then
-        IP_ADDR="10.10.${id}.1/24"
-    elif [ ${HOSTNAME} == "asus" ]; then
-        IP_ADDR="10.10.${id}.1/24"
-    elif [ ${HOSTNAME} == "x1" ]; then
-        IP_ADDR="10.10.${id}.3/24"
-    else
-        echo "Can not run in host: ${HOSTNAME}"
-        IP_ADDR=""
-    fi
+    # Set IP address based on host mapping
+    IP_ADDR="${NETWORK_PREFIX}.${id}.${HOST_IP}/24"
 
     # Check if bridge exists
     if ! bridge_exists $NET_NAME; then
         echo "Creating bridge $NET_NAME"
         sudo brctl addbr $NET_NAME
 
-        # Check if VLAN is already part of a bridge
-        if ! ip link show vlan${id} | grep -q "master"; then
-            echo "Adding vlan${id} to bridge $NET_NAME"
-            sudo brctl addif $NET_NAME vlan${id}
+        # Add physical interface to bridge if mapped
+        if [ -n "${DEVICE_MAP[$br]}" ]; then
+            if ! ip link show vlan${id} | grep -q "master"; then
+                echo "Adding vlan${id} to bridge $NET_NAME"
+                sudo brctl addif $NET_NAME vlan${id}
+            else
+                echo "VLAN ${id} is already part of a bridge, skipping addition"
+            fi
         else
-            echo "VLAN ${id} is already part of a bridge, skipping addition"
+            echo "No physical device mapped to bridge $NET_NAME, creating standalone bridge"
         fi
 
         if [ -n "$IP_ADDR" ]; then
@@ -91,14 +182,17 @@ for br in oam admin public internal ext k8s; do
     else
         echo "Bridge $NET_NAME already exists, checking configuration"
 
-        # Check if VLAN is already part of this bridge
-        if ! ip link show vlan${id} | grep -q "master $NET_NAME"; then
-            # Check if VLAN is part of another bridge
-            if ip link show vlan${id} | grep -q "master"; then
-                echo "VLAN ${id} is already part of another bridge, please check configuration manually"
-            else
-                echo "Adding vlan${id} to bridge $NET_NAME"
-                sudo brctl addif $NET_NAME vlan${id}
+        # Only manage VLAN interface if this bridge has a physical device mapped
+        if [ -n "${DEVICE_MAP[$br]}" ]; then
+            # Check if VLAN is already part of this bridge
+            if ! ip link show vlan${id} | grep -q "master $NET_NAME"; then
+                # Check if VLAN is part of another bridge
+                if ip link show vlan${id} | grep -q "master"; then
+                    echo "VLAN ${id} is already part of another bridge, please check configuration manually"
+                else
+                    echo "Adding vlan${id} to bridge $NET_NAME"
+                    sudo brctl addif $NET_NAME vlan${id}
+                fi
             fi
         fi
 
@@ -121,46 +215,63 @@ for br in oam admin public internal ext k8s; do
     fi
 
     # Check if firewall rules already exist
-    if ! sudo iptables -C FORWARD -s 10.10.${id}.0/24 -j ACCEPT &>/dev/null; then
-        echo "Adding firewall rule for source network 10.10.${id}.0/24"
-        sudo iptables -I FORWARD -s 10.10.${id}.0/24 -j ACCEPT
+    NETWORK="${NETWORK_PREFIX}.${id}.0/24"
+    
+    if ! sudo iptables -C FORWARD -s ${NETWORK} -j ACCEPT &>/dev/null; then
+        echo "Adding firewall rule for source network ${NETWORK}"
+        sudo iptables -I FORWARD -s ${NETWORK} -j ACCEPT
     else
-        echo "Firewall rule for source network 10.10.${id}.0/24 already exists"
+        echo "Firewall rule for source network ${NETWORK} already exists"
     fi
 
-    if ! sudo iptables -C FORWARD -d 10.10.${id}.0/24 -j ACCEPT &>/dev/null; then
-        echo "Adding firewall rule for destination network 10.10.${id}.0/24"
-        sudo iptables -I FORWARD -d 10.10.${id}.0/24 -j ACCEPT
+    if ! sudo iptables -C FORWARD -d ${NETWORK} -j ACCEPT &>/dev/null; then
+        echo "Adding firewall rule for destination network ${NETWORK}"
+        sudo iptables -I FORWARD -d ${NETWORK} -j ACCEPT
     else
-        echo "Firewall rule for destination network 10.10.${id}.0/24 already exists"
+        echo "Firewall rule for destination network ${NETWORK} already exists"
     fi
 
-    if ! sudo iptables -t nat -C POSTROUTING -o ${NETDEV} -s 10.10.${id}.0/24 -j MASQUERADE &>/dev/null; then
-        echo "Adding NAT masquerade rule for network 10.10.${id}.0/24"
-        sudo iptables -t nat -I POSTROUTING -o ${NETDEV} -s 10.10.${id}.0/24 -j MASQUERADE
+    # Only add NAT rules if this bridge has a physical device mapped
+    if [ -n "${DEVICE_MAP[$br]}" ]; then
+        if ! sudo iptables -t nat -C POSTROUTING -o ${DEVICE_MAP[$br]} -s ${NETWORK} -j MASQUERADE &>/dev/null; then
+            echo "Adding NAT masquerade rule for network ${NETWORK}"
+            sudo iptables -t nat -I POSTROUTING -o ${DEVICE_MAP[$br]} -s ${NETWORK} -j MASQUERADE
+        else
+            echo "NAT masquerade rule for network ${NETWORK} already exists"
+        fi
     else
-        echo "NAT masquerade rule for network 10.10.${id}.0/24 already exists"
+        echo "No physical device mapped to bridge $br, skipping NAT masquerade rule"
     fi
 
     # Check if virsh network exists
-    if ! virsh_net_exists $NET_NAME; then
-        echo "Creating virsh network $NET_NAME"
-        virsh net-define maas-net-${br}.xml
-        virsh net-start $NET_NAME
-        virsh net-autostart $NET_NAME
-    else
-        echo "Virsh network $NET_NAME already exists"
-        if ! virsh net-info $NET_NAME | grep -q "Active:.*yes"; then
-            echo "Starting virsh network $NET_NAME"
-            virsh net-start $NET_NAME
-        fi
-        if ! virsh net-info $NET_NAME | grep -q "Autostart:.*yes"; then
-            echo "Setting virsh network $NET_NAME to autostart"
-            virsh net-autostart $NET_NAME
-        fi
+    XML_FILE="${XML_OUTPUT_DIR}/net-${br}.xml"
+    
+    # Create XML file if it doesn't exist
+    if [ ! -f "$XML_FILE" ]; then
+        create_network_xml "$br"
     fi
-
-    id=$((id+10))
+    
+    if [ -f "$XML_FILE" ]; then
+        if ! virsh_net_exists $NET_NAME; then
+            echo "Creating virsh network $NET_NAME from $XML_FILE"
+            virsh net-define "$XML_FILE"
+            virsh net-start $NET_NAME
+            virsh net-autostart $NET_NAME
+        else
+            echo "Virsh network $NET_NAME already exists"
+            if ! virsh net-info $NET_NAME | grep -q "Active:.*yes"; then
+                echo "Starting virsh network $NET_NAME"
+                virsh net-start $NET_NAME
+            fi
+            if ! virsh net-info $NET_NAME | grep -q "Autostart:.*yes"; then
+                echo "Setting virsh network $NET_NAME to autostart"
+                virsh net-autostart $NET_NAME
+            fi
+        fi
+    else
+        echo "Error: Failed to create XML configuration file for $NET_NAME"
+        exit 1
+    fi
 done
 
 echo "Network setup completed successfully"
