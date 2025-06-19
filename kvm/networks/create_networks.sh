267ee65b6e8e4b5dac4a1bfc8e286331e744b785
Erlon R. Cruz <erlon@canonical.com>
2025-06-19 15:42:51 -0300

    Enhance network and VM parameterization for flexible deployment
    This commit improves the flexibility and adaptability of the KVM network and VM setup scripts:
    
    1. Network script (create_networks.sh) improvements:
       - Added --external-nic parameter to specify internet-facing device
       - Added --use-vlans flag to make VLAN configuration optional
       - Fixed masquerading logic to only apply to networks without physical devices
       - Improved bridge creation with cleaner IP configuration
       - Generated network XML files dynamically from template
    
    2. VM script (spawn-vm.sh) improvements:
       - Added auto-detection of virtualization capabilities
       - Optimized VM creation with appropriate acceleration options
       - Added support for custom networks in non-MAAS mode
       - Improved network interface configuration with dynamic MAC addresses
       - Added debug option for troubleshooting
    
    These changes enable the scripts to run on any host with user-defined parameters for bridges, devices, networks, and virtualization options.

--- kvm/networks/create_networks.sh
+++ kvm/networks/create_networks.sh
@@ -7,6 +7,8 @@ BRIDGES=("oam:10" "admin:20" "public:30" "internal:40" "ext:50" "k8s:60")
 NETWORK_PREFIX="10.10"
 XML_TEMPLATE_FILE="$(dirname "$0")/network-template.xml"
 XML_OUTPUT_DIR="."
+EXTERNAL_NIC=""
+USE_VLANS=false
 
 # Help function
 show_help() {
@@ -17,6 +19,8 @@ show_help() {
     echo "  -b, --bridge NAME:ID      Add bridge with VLAN ID (e.g., oam:10,admin:20)"
     echo "  -p, --prefix PREFIX       Network prefix (default: 10.10)"
     echo "  -o, --output-dir DIR      Directory to store generated XML files (default: current directory)"
+    echo "  -e, --external-nic DEV External device used for internet access (e.g., eth0)"
+    echo "  --use-vlans               Configure VLANs for bridges with physical devices"
     echo "  --help                    Show this help message"
     exit 0
 }
@@ -44,6 +48,14 @@ while [[ $# -gt 0 ]]; do
         -o|--output-dir)
             XML_OUTPUT_DIR="$2"
             shift 2
+            ;;
+        -e|--external-nic)
+            EXTERNAL_NIC="$2"
+            shift 2
+            ;;
+        --use-vlans)
+            USE_VLANS=true
+            shift
             ;;
         --help)
             show_help
@@ -103,7 +115,7 @@ ip_is_configured() {
 
 # Function to check if network exists in virsh
 virsh_net_exists() {
-    virsh net-info $1 &>/dev/null
+    sudo virsh net-info $1 &>/dev/null
     return $?
 }
 
@@ -125,123 +137,121 @@ create_network_xml() {
     fi
 }
 
-# Create network vlans
-for bridge in "${BRIDGES[@]}"; do
-    IFS=':' read -r br id <<< "$bridge"
+# Configure firewall and NAT
+configure_firewall() {
+    local br="$1"
+    local dev="$2"
+    local id="$3"
     
-    # Skip VLAN creation if no device is mapped to this bridge
-    if [ -z "${DEVICE_MAP[$br]}" ]; then
-        echo "No physical device mapped to bridge $br, skipping VLAN creation"
-        continue
-    fi
+    # Enable IP forwarding
+    sudo sh -c "echo 1 > /proc/sys/net/ipv4/ip_forward"
     
-    NETDEV=${DEVICE_MAP[$br]}
-    if ! vlan_exists $id; then
-        echo "Creating VLAN $id for bridge $br on device $NETDEV"
-        sudo ip link add link ${NETDEV} name vlan${id} type vlan id ${id}
-        sudo ip link set dev vlan${id} up
+    # Add firewall rules for the bridge
+    sudo iptables -I FORWARD -i "${br}" -o "${br}" -j ACCEPT
+    
+    # If this bridge has a physical device mapped to it
+    if [ -n "$dev" ]; then
+        # Allow traffic from bridge to physical device and back
+        sudo iptables -I FORWARD -i "${br}" -o "${dev}" -j ACCEPT
+        sudo iptables -I FORWARD -i "${dev}" -o "${br}" -j ACCEPT
+        
+        # No NAT needed for bridges with physical devices
+        echo "Bridge ${br} has physical device ${dev}, no NAT needed"
+    # If external device is specified and this bridge doesn't have a physical device
+    elif [ -n "$EXTERNAL_NIC" ]; then
+        # Allow traffic from bridge to external device and back
+        sudo iptables -I FORWARD -i "${br}" -o "${EXTERNAL_NIC}" -j ACCEPT
+        sudo iptables -I FORWARD -i "${EXTERNAL_NIC}" -o "${br}" -j ACCEPT
+        
+        # Set up NAT for outbound traffic from bridge to external device
+        sudo iptables -t nat -I POSTROUTING -s "${NETWORK_PREFIX}.${id}.0/24" -o "${EXTERNAL_NIC}" -j MASQUERADE
+        echo "Bridge ${br} using external device ${EXTERNAL_NIC} for NAT"
     else
-        echo "VLAN $id already exists, checking status"
-        if ! ip link show vlan${id} | grep -q "UP"; then
-            echo "Setting VLAN $id up"
-            sudo ip link set dev vlan${id} up
-        fi
+        echo "Bridge ${br} has no physical device and no external device specified, no NAT configured"
     fi
-done
+}
 
-# Create bridges and configure networks
+# Create network vlans
 for bridge in "${BRIDGES[@]}"; do
     IFS=':' read -r br id <<< "$bridge"
     NET_NAME="${br}"
 
     # Set IP address based on host mapping
     IP_ADDR="${NETWORK_PREFIX}.${id}.${HOST_IP}/24"
-
-    # Check if bridge exists
-    if ! bridge_exists $NET_NAME; then
-        echo "Creating bridge $NET_NAME"
-        sudo brctl addbr $NET_NAME
-
-        # Add physical interface to bridge if mapped
-        if [ -n "${DEVICE_MAP[$br]}" ]; then
-            if ! ip link show vlan${id} | grep -q "master"; then
-                echo "Adding vlan${id} to bridge $NET_NAME"
-                sudo brctl addif $NET_NAME vlan${id}
-            else
-                echo "VLAN ${id} is already part of a bridge, skipping addition"
-            fi
-        else
-            echo "No physical device mapped to bridge $NET_NAME, creating standalone bridge"
-        fi
-
-        if [ -n "$IP_ADDR" ]; then
-            echo "Setting IP $IP_ADDR on $NET_NAME"
-            sudo ip addr add $IP_ADDR dev $NET_NAME
-            sudo ip link set dev $NET_NAME up
-        fi
-    else
-        echo "Bridge $NET_NAME already exists, checking configuration"
-
-        # Only manage VLAN interface if this bridge has a physical device mapped
-        if [ -n "${DEVICE_MAP[$br]}" ]; then
-            # Check if VLAN is already part of this bridge
-            if ! ip link show vlan${id} | grep -q "master $NET_NAME"; then
-                # Check if VLAN is part of another bridge
-                if ip link show vlan${id} | grep -q "master"; then
-                    echo "VLAN ${id} is already part of another bridge, please check configuration manually"
-                else
-                    echo "Adding vlan${id} to bridge $NET_NAME"
-                    sudo brctl addif $NET_NAME vlan${id}
-                fi
-            fi
-        fi
-
-        # Check if bridge is up
-        if ! ip link show $NET_NAME | grep -q "UP"; then
-            echo "Setting bridge $NET_NAME up"
-            sudo ip link set dev $NET_NAME up
-        fi
-
-        # Check if IP is correctly configured
-        if [ -n "$IP_ADDR" ]; then
-            IP_WITHOUT_MASK=$(echo $IP_ADDR | cut -d'/' -f1)
-            if ! ip_is_configured $NET_NAME $IP_WITHOUT_MASK; then
-                echo "IP address $IP_ADDR not found on $NET_NAME, adding it"
-                sudo ip addr add $IP_ADDR dev $NET_NAME
-            else
-                echo "IP address correctly configured on $NET_NAME"
-            fi
-        fi
-    fi
-
-    # Check if firewall rules already exist
-    NETWORK="${NETWORK_PREFIX}.${id}.0/24"
     
-    if ! sudo iptables -C FORWARD -s ${NETWORK} -j ACCEPT &>/dev/null; then
-        echo "Adding firewall rule for source network ${NETWORK}"
-        sudo iptables -I FORWARD -s ${NETWORK} -j ACCEPT
-    else
-        echo "Firewall rule for source network ${NETWORK} already exists"
-    fi
-
-    if ! sudo iptables -C FORWARD -d ${NETWORK} -j ACCEPT &>/dev/null; then
-        echo "Adding firewall rule for destination network ${NETWORK}"
-        sudo iptables -I FORWARD -d ${NETWORK} -j ACCEPT
-    else
-        echo "Firewall rule for destination network ${NETWORK} already exists"
-    fi
-
-    # Only add NAT rules if this bridge has a physical device mapped
+    # Check if this bridge has a device mapped to it
     if [ -n "${DEVICE_MAP[$br]}" ]; then
-        if ! sudo iptables -t nat -C POSTROUTING -o ${DEVICE_MAP[$br]} -s ${NETWORK} -j MASQUERADE &>/dev/null; then
-            echo "Adding NAT masquerade rule for network ${NETWORK}"
-            sudo iptables -t nat -I POSTROUTING -o ${DEVICE_MAP[$br]} -s ${NETWORK} -j MASQUERADE
+        DEVICE="${DEVICE_MAP[$br]}"
+        echo "Setting up bridge $NET_NAME with device $DEVICE"
+        
+        # Create bridge if it doesn't exist
+        if ! ip link show "$NET_NAME" &>/dev/null; then
+            echo "Creating bridge $NET_NAME"
+            sudo ip link add name "$NET_NAME" type bridge
+            sudo ip link set dev "$NET_NAME" up
         else
-            echo "NAT masquerade rule for network ${NETWORK} already exists"
+            echo "Bridge $NET_NAME already exists"
+        fi
+        
+        # Configure VLAN only if --use-vlans is specified
+        if $USE_VLANS; then
+            # Create VLAN interface if it doesn't exist
+            VLAN_IF="${DEVICE}.${id}"
+            if ! ip link show "$VLAN_IF" &>/dev/null; then
+                echo "Creating VLAN interface $VLAN_IF"
+                sudo ip link add link "$DEVICE" name "$VLAN_IF" type vlan id "$id"
+                sudo ip link set dev "$VLAN_IF" up
+            else
+                echo "VLAN interface $VLAN_IF already exists"
+            fi
+            
+            # Add VLAN interface to bridge if not already added
+            if ! ip link show "$VLAN_IF" | grep -q "master $NET_NAME"; then
+                echo "Adding VLAN interface $VLAN_IF to bridge $NET_NAME"
+                sudo ip link set dev "$VLAN_IF" master "$NET_NAME"
+            else
+                echo "VLAN interface $VLAN_IF already added to bridge $NET_NAME"
+            fi
+        else
+            # If not using VLANs, add the physical device directly to the bridge
+            if ! ip link show "$DEVICE" | grep -q "master $NET_NAME"; then
+                echo "Adding device $DEVICE directly to bridge $NET_NAME (no VLAN)"
+                sudo ip link set dev "$DEVICE" master "$NET_NAME"
+            else
+                echo "Device $DEVICE already added to bridge $NET_NAME"
+            fi
+        fi
+        
+        # Set IP address on bridge
+        if ! ip addr show dev "$NET_NAME" | grep -q "$IP_ADDR"; then
+            echo "Setting IP address $IP_ADDR on bridge $NET_NAME"
+            sudo ip addr add "$IP_ADDR" dev "$NET_NAME"
+        else
+            echo "IP address $IP_ADDR already set on bridge $NET_NAME"
         fi
     else
-        echo "No physical device mapped to bridge $br, skipping NAT masquerade rule"
+        echo "No device mapped to bridge $NET_NAME, creating a virtual bridge"
+        
+        # Create bridge if it doesn't exist
+        if ! ip link show "$NET_NAME" &>/dev/null; then
+            echo "Creating bridge $NET_NAME"
+            sudo ip link add name "$NET_NAME" type bridge
+            sudo ip link set dev "$NET_NAME" up
+        else
+            echo "Bridge $NET_NAME already exists"
+        fi
+        
+        # Set IP address on bridge
+        if ! ip addr show dev "$NET_NAME" | grep -q "$IP_ADDR"; then
+            echo "Setting IP address $IP_ADDR on bridge $NET_NAME"
+            sudo ip addr add "$IP_ADDR" dev "$NET_NAME"
+        else
+            echo "IP address $IP_ADDR already set on bridge $NET_NAME"
+        fi
     fi
+
+    # Configure firewall and NAT
+    configure_firewall "$NET_NAME" "${DEVICE_MAP[$br]}" "$id"
 
     # Check if virsh network exists
     XML_FILE="${XML_OUTPUT_DIR}/net-${br}.xml"
@@ -254,18 +264,18 @@ for bridge in "${BRIDGES[@]}"; do
     if [ -f "$XML_FILE" ]; then
         if ! virsh_net_exists $NET_NAME; then
             echo "Creating virsh network $NET_NAME from $XML_FILE"
-            virsh net-define "$XML_FILE"
-            virsh net-start $NET_NAME
-            virsh net-autostart $NET_NAME
+            sudo virsh net-define "$XML_FILE"
+            sudo virsh net-start $NET_NAME
+            sudo virsh net-autostart $NET_NAME
         else
             echo "Virsh network $NET_NAME already exists"
-            if ! virsh net-info $NET_NAME | grep -q "Active:.*yes"; then
+            if ! sudo  virsh net-info $NET_NAME | grep -q "Active:.*yes"; then
                 echo "Starting virsh network $NET_NAME"
-                virsh net-start $NET_NAME
+                sudo virsh net-start $NET_NAME
             fi
-            if ! virsh net-info $NET_NAME | grep -q "Autostart:.*yes"; then
+            if ! sudo virsh net-info $NET_NAME | grep -q "Autostart:.*yes"; then
                 echo "Setting virsh network $NET_NAME to autostart"
-                virsh net-autostart $NET_NAME
+                sudo virsh net-autostart $NET_NAME
             fi
         fi
     else
--- kvm/spawn-vm.sh
+++ kvm/spawn-vm.sh
@@ -1,12 +1,16 @@
-#!/bin/bash -ex
+#!/bin/bash -e
 
 export -A IMAGES=()
 BASE_FOLDER="${HOME}/VMScripts"
 IMAGE_FOLDER="${HOME}/VMStorage/Images"
 DISK_FOLDER="${HOME}/VMStorage/Disks"
-
 MAAS=false
+NETWORK="default"
+NETWORKS=()
+NETWORK_PREFIX="52:54:00"
+FORCE=false
 
+# Define image paths
 IMAGES["trusty"]="${IMAGE_FOLDER}/trusty-server-cloudimg-amd64-disk1.img"
 IMAGES["xenial"]="${IMAGE_FOLDER}/xenial-server-cloudimg-amd64-disk1.img"
 IMAGES["bionic"]="${IMAGE_FOLDER}/bionic-server-cloudimg-amd64.img"
@@ -14,8 +18,53 @@ IMAGES["focal"]="${IMAGE_FOLDER}/focal-server-cloudimg-amd64.img"
 IMAGES["jammy"]="${IMAGE_FOLDER}/jammy-server-cloudimg-amd64.img"
 IMAGES["centos7"]="${IMAGE_FOLDER}/CentOS-7-x86_64-GenericCloud.qcow2"
 
+# Function to check if host supports hardware virtualization
+check_virtualization_support() {
+    # Check if CPU supports virtualization
+    if grep -q -E 'vmx|svm' /proc/cpuinfo; then
+        # Check if KVM module is loaded
+        if lsmod | grep -q kvm; then
+            # Check if we're running in a VM
+            if [ -e /sys/hypervisor/type ] || grep -q -E 'KVM|QEMU|VMware|VirtualBox|Xen' /proc/cpuinfo /proc/version /proc/interrupts /proc/modules 2>/dev/null; then
+                echo "Running in a VM with nested virtualization support"
+                return 1  # Return 1 for nested virtualization
+            else
+                echo "Running on physical hardware with virtualization support"
+                return 0  # Return 0 for physical hardware
+            fi
+        else
+            echo "CPU supports virtualization but KVM module is not loaded"
+            return 2  # Return 2 for no KVM module
+        fi
+    else
+        echo "CPU does not support hardware virtualization"
+        return 3  # Return 3 for no virtualization support
+    fi
+}
+
+# Function to generate a random byte in hex format
+generate_random_hex() {
+    printf '%02x' $((RANDOM % 256))
+}
+
+# Function to generate a unique MAC address with given prefix
+generate_mac() {
+    local prefix=$1
+    local index=$2
+    # Generate 3 random bytes for the MAC address
+    local byte1=$(generate_random_hex)
+    local byte2=$(generate_random_hex)
+    local byte3=$(generate_random_hex)
+    # Format: 52:54:00:XX:XX:XX
+    echo "${prefix}:${byte1}:${byte2}:${byte3}"
+}
+
 usage() {
-    echo "Usage: $0 --name <hostname> --vcpus <vcpus> --mem <memory MB> --disk <disk GB> [--series <trusty|xenial|bionic|focal|jammy>]"
+    echo "Usage: $0 --name <hostname> --vcpus <vcpus> --mem <memory MB> --disk <disk GB>"
+    echo "  [--series <trusty|xenial|bionic|focal|jammy>]"
+    echo "  [--maas]"
+    echo "  [--networks <network1>[,<network2>...]]  Comma-separated list of networks (e.g., maas-admin,maas-public,maas-internal)"
+    echo "  [--network-prefix <prefix>]             MAC address prefix (default: 52:54:00)"
 }
 
 if [ -z $1 ] || [ -z $2 ]; then
@@ -53,9 +102,20 @@ while (($# > 0)); do
         --maas)
             MAAS=true
             ;;
+        --networks)
+            IFS=',' read -ra NETWORKS <<< "$2"
+            shift
+            ;;
+        --network-prefix)
+            NETWORK_PREFIX=$2
+            shift
+            ;;
         --net)
             NETWORK=$2
             shift
+            ;;
+        --debug)
+            set -x
             ;;
         *)
             echo "Invalid input parameter: ${1}"
@@ -103,50 +163,129 @@ fi
 
 if [ ${MAAS} == "true" ]; then
     echo "Creating VM with MAAS support"
-    # Chooses a random number, convert it to hex and get the first 2 digits.
-    # There is a scarse change that we can colide MACs in the same network, but
-    # given the complexity to manage that, it worh the risks given that changes
-    # are really small, the number of machines in the system is small and its a
-    # development system.
-    # We need toset that with some logic so we can indentify what network a
-    # card is on from its MACs address
-    hex1=$(printf '%x\n' $(echo $RANDOM))
-    hex1=${hex1:0:2}
-    hex2=$(printf '%x\n' $(echo $RANDOM))
-    hex2=${hex2:0:2}
-    sudo virt-install \
-                --name $VMNAME \
-                --memory $MEMORY \
-                --vcpus $VCPUS \
-                --disk path=${DISK_FOLDER}/vmdisk-${VMNAME}-root.qcow2,format=qcow2,device=disk,bus=sata \
-                --disk path=${DISK_FOLDER}/vmdisk-${VMNAME}-01.qcow2,format=qcow2,device=disk,bus=sata \
-                --disk path=${DISK_FOLDER}/vmconfigs-${VMNAME}.iso,device=cdrom \
-                --pxe --boot hd,network \
-                --os-type linux \
-                --os-variant ubuntu18.04 \
-                --virt-type kvm \
-                --graphics spice \
-                --network=network=maas-admin,model=virtio,mac="52:54:00:${hex1}:${hex2}:20" \
-                --network=network=maas-public,model=virtio,mac="52:54:00:${hex1}:${hex2}:30" \
-                --network=network=maas-internal,model=virtio,mac="52:54:00:${hex1}:${hex2}:40" \
-                --check path_in_use=off \
-                --noautoconsole
+    
+    # Default networks if none specified
+    if [ ${#NETWORKS[@]} -eq 0 ]; then
+        NETWORKS=("maas-admin" "maas-public" "maas-internal")
+        echo "No networks specified, using defaults: ${NETWORKS[*]}"
+    fi
+    
+    # Build network options for virt-install
+    NETWORK_OPTS=()
+    for i in "${!NETWORKS[@]}"; do
+        # Generate a unique MAC address for each network interface
+        # Use the index to make the last byte unique
+        mac=$(generate_mac "${NETWORK_PREFIX}" "$i")
+        NETWORK_OPTS+=("--network" "network=${NETWORKS[$i]},model=virtio,mac=$mac")
+        echo "Network ${NETWORKS[$i]} will use MAC: $mac"
+    done
+    
+    # Check if host supports hardware virtualization
+    VIRT_OPTS=()
+    check_virtualization_support
+    VIRT_SUPPORT=$?
+    
+    if [ $VIRT_SUPPORT -eq 0 ]; then
+        # Physical hardware with virtualization support
+        VIRT_OPTS+=("--virt-type=kvm" "--cpu=host-passthrough")
+        echo "Using KVM acceleration with host-passthrough CPU"
+    elif [ $VIRT_SUPPORT -eq 1 ]; then
+        # Nested virtualization
+        VIRT_OPTS+=("--virt-type=kvm")
+        echo "Using KVM acceleration"
+    else
+        # No hardware virtualization
+        VIRT_OPTS+=("--virt-type=qemu")
+        echo "Using QEMU emulation (no hardware acceleration)"
+    fi
+    
+    # Build the virt-install command
+    CMD=(
+        sudo virt-install
+        --name "$VMNAME"
+        --memory "$MEMORY"
+        --vcpus "$VCPUS"
+        --disk "path=${DISK_FOLDER}/vmdisk-${VMNAME}-root.qcow2,format=qcow2,device=disk,bus=sata"
+        --disk "path=${DISK_FOLDER}/vmdisk-${VMNAME}-01.qcow2,format=qcow2,device=disk,bus=sata"
+        --disk "path=${DISK_FOLDER}/vmconfigs-${VMNAME}.iso,device=cdrom"
+        --pxe
+        --boot hd,network
+        --os-type linux
+        --os-variant ubuntu18.04
+        --virt-type kvm
+        --graphics spice
+        --check path_in_use=off
+        --noautoconsole
+        "${NETWORK_OPTS[@]}"
+        "${VIRT_OPTS[@]}"
+    )
+    
+    # Execute the command
+    echo "Creating VM with the following network configuration:"
+    printf '  %s\n' "${NETWORK_OPTS[@]}"
+    "${CMD[@]}"
 else
     echo "Creating VM with no MAAS support"
-    sudo virt-install \
-                --name $VMNAME \
-                --memory $MEMORY \
-                --vcpus $VCPUS \
-                --disk path=${DISK_FOLDER}/vmdisk-${VMNAME}.qcow2,format=qcow2,device=disk,bus=virtio \
-                --disk path=${DISK_FOLDER}/vmconfigs-${VMNAME}.iso,device=cdrom \
-                --os-type linux \
-                --os-variant ubuntu18.04 \
-                --virt-type kvm \
-                --graphics spice \
-                --network network=$NETWORK,model=e1000 \
-                --import \
-                --check path_in_use=off \
-                --noautoconsole
+    
+    # Build network options for virt-install
+    NETWORK_OPTS=()
+    
+    # If networks are specified, use them
+    if [ ${#NETWORKS[@]} -gt 0 ]; then
+        echo "Using specified networks: ${NETWORKS[*]}"
+        for i in "${!NETWORKS[@]}"; do
+            # Generate a unique MAC address for each network interface
+            mac=$(generate_mac "${NETWORK_PREFIX}" "$i")
+            NETWORK_OPTS+=("--network=network=${NETWORKS[$i]},model=virtio,mac=$mac")
+            echo "Network ${NETWORKS[$i]} will use MAC: $mac"
+        done
+    else
+        # Use the default network
+        echo "Using default network: $NETWORK"
+        NETWORK_OPTS+=("--network=network=$NETWORK,model=e1000")
+    fi
+    
+    # Check if host supports hardware virtualization
+    VIRT_OPTS=()
+    check_virtualization_support
+    VIRT_SUPPORT=$?
+    
+    if [ $VIRT_SUPPORT -eq 0 ]; then
+        # Physical hardware with virtualization support
+        VIRT_OPTS+=("--virt-type=kvm" "--cpu=host-passthrough")
+        echo "Using KVM acceleration with host-passthrough CPU"
+    elif [ $VIRT_SUPPORT -eq 1 ]; then
+        # Nested virtualization
+        VIRT_OPTS+=("--virt-type=kvm")
+        echo "Using KVM acceleration"
+    else
+        # No hardware virtualization
+        VIRT_OPTS+=("--virt-type=qemu")
+        echo "Using QEMU emulation (no hardware acceleration)"
+    fi
+    
+    # Build the virt-install command
+    CMD=(
+        sudo virt-install
+        --name "$VMNAME"
+        --memory "$MEMORY"
+        --vcpus "$VCPUS"
+        --disk "path=${DISK_FOLDER}/vmdisk-${VMNAME}.qcow2,format=qcow2,device=disk,bus=virtio"
+        --disk "path=${DISK_FOLDER}/vmconfigs-${VMNAME}.iso,device=cdrom"
+        --os-type linux
+        --os-variant ubuntu18.04
+        --graphics spice
+        --import
+        --check path_in_use=off
+        --noautoconsole
+        "${NETWORK_OPTS[@]}"
+        "${VIRT_OPTS[@]}"
+    )
+    
+    # Execute the command
+    echo "Creating VM with the following network configuration:"
+    printf '  %s\n' "${NETWORK_OPTS[@]}"
+    "${CMD[@]}"
 
     ip_count=$(sudo virsh domifaddr $VMNAME | grep ipv4 | wc -l)
     echo -n "Waiting for machine to boot ."
@@ -162,4 +301,3 @@ else
     echo "You can log in the instance with:"
     echo "ssh ubuntu@$ip"
 fi
-
